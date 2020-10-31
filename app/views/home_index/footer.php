6b10461aa741282e563321a7df3f883cfed50bc2
Imaduddin Syukra <33992229+imaduddinsyukra@users.noreply.github.com>
2020-10-31 10:41:49 +0700

    Update footer.php

--- app/views/home_index/footer.php
+++ app/views/home_index/footer.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="footer-copy-right">
-                        <p>Copyright ©2020 Badan Amil Zakat Nurul Hidayah. All rights reserved.</p>
+                        <p>Copyright ©2020 <a href="http://www.kampus-programmer.com/"><b>Kampus Programmer.</b></a> All rights reserved.</p>
                     </div>
                 </div>
             </div>
